5650a04569e0efdeb607684883abed56f257e409
Robert Simpson <robert_simpson@hms.harvard.edu>
2016-03-29 16:16:15 -0400

    Install lorem-ipsum package, add practice route.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,15 @@
 Route::group(['middleware' => ['web']], function () {
 
     Route::get('/', function () {
-        return view('welcome');
+        return 'Welcome to P3 by Robert Simpson';
+    });
+
+    Route::get('/practice',function() {
+
+        $generator = new Badcow\LoremIpsum\Generator();
+        $paragraphs = $generator->getParagraphs(3);
+        echo implode('<p>', $paragraphs);
+        return '';
     });
 
 });
